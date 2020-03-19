a242e64ea098e04880056b4cc04637a2a2cd5b0c
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-03-19 13:57:29 +0200

    Improvments to client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return <div className="test">Andrei</div>;
+    }
+}
+
+export default App;

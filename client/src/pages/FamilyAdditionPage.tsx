9632b5df7a2d5f23d70da0e2866a751c1dc6d58b
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-03-25 16:34:34 +0200

    Client made creating new family better

--- client/src/pages/FamilyAdditionPage.tsx
+++ client/src/pages/FamilyAdditionPage.tsx
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { ButtonBigAlt } from '../components/Button';
 import { newFamily } from '../services/familyService';
+import { Redirect } from 'react-router-dom';
 
-export default class FamilyAdditionPage extends Component {
+interface state {
+    redirect: boolean;
+}
+
+export default class FamilyAdditionPage extends PureComponent<{}, state> {
     newFamily: React.RefObject<HTMLInputElement>;
     constructor(props: Readonly<{}>) {
         super(props);
         this.newFamily = React.createRef();
+
+        this.state = {
+            redirect: false
+        };
     }
 
     submitNewFamily = async () => {
-        newFamily(this.newFamily.current.value);
-        console.log('Done');
+        // TODO: Put this into an utility file
+        // Remove unnecessary spaces
+        let newFamilyName = this.newFamily.current.value.replace(
+            / +(?= )/g,
+            ''
+        );
+        // Error checking
+        if (newFamilyName !== '' && newFamilyName !== ' ') {
+            newFamily(this.newFamily.current.value).then(() =>
+                this.setState({
+                    redirect: true
+                })
+            );
+        }
     };
 
     render() {
+        if (this.state.redirect) {
+            return <Redirect to="/" />;
+        }
         return (
             <div className="addition-page-wrap">
                 <div className="addition-content-wrap">

5a67162aa197961029b0d8be7b49627f2004bace
Erkki Sula <Erkki.Sula@netgroup.com>
2020-06-17 14:16:52 +0300

    Converted last few components to use hooks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import Router from './components/Router';
 
-class App extends React.Component {
-    render() {
-        return <Router />;
-    }
-}
+const App = () => {
+    return <Router />;
+};
 
 export default App;
--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,76 +1,57 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { IoMdSearch } from 'react-icons/io';
 import { PersonType } from '../types/PersonType';
 import { PersonHeader } from '../components/Person';
 import PeopleTable from '../components/PeopleTable';
 import { searchPeopleByQuery } from '../services/personService';
 
-interface PeoplePageState {
-    items: PersonType[];
-}
+export const SearchPage = () => {
+    const searchRef: React.RefObject<HTMLInputElement> = React.createRef();
+    const [items, setItems] = useState<PersonType[]>([]);
 
-export default class SearchPage extends PureComponent<{}, PeoplePageState> {
-    searchRef: React.RefObject<HTMLInputElement>;
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            items: []
-        };
-        this.searchRef = React.createRef();
-    }
-
-    searchDatabase = async () => {
+    const searchDatabase = async () => {
         // Remove unnecessary spaces
-        let searchQuery = this.searchRef.current.value.replace(/ +(?= )/g, '');
+        let searchQuery = searchRef.current.value.replace(/ +(?= )/g, '');
         // Error checking
         if (searchQuery !== '' && searchQuery !== ' ') {
             // Removing error causing symbol
-            await searchPeopleByQuery(searchQuery.replace('+', '')).then(
-                data => {
-                    this.setState({
-                        items: data
-                    });
-                }
-            );
-        } else {
-            this.setState({
-                items: []
+            await searchPeopleByQuery(searchQuery.replace('+', '')).then((data) => {
+                setItems(data);
             });
+        } else {
+            setItems([]);
         }
     };
 
-    render() {
-        return (
-            <div>
-                <div className="people-wrap">
-                    <div className="people-content-wrap">
-                        <div className="people-search-panel">
-                            <div className="people-search-panel-input-wrap">
-                                <div className="people-search-body">
-                                    <input
-                                        type="text"
-                                        name="people-search-input"
-                                        id="people-search-input"
-                                        className="people-search-input"
-                                        placeholder="Search..."
-                                        ref={this.searchRef}
-                                    />
-                                    <div
-                                        className="people-search-btn"
-                                        onClick={() => this.searchDatabase()}
-                                    >
-                                        <IoMdSearch />
-                                    </div>
+    return (
+        <div>
+            <div className="people-wrap">
+                <div className="people-content-wrap">
+                    <div className="people-search-panel">
+                        <div className="people-search-panel-input-wrap">
+                            <div className="people-search-body">
+                                <input
+                                    type="text"
+                                    name="people-search-input"
+                                    id="people-search-input"
+                                    className="people-search-input"
+                                    placeholder="Search..."
+                                    ref={searchRef}
+                                />
+                                <div className="people-search-btn" onClick={() => searchDatabase()}>
+                                    <IoMdSearch />
                                 </div>
                             </div>
                         </div>
-                        <PersonHeader />
-                        <div className="people-table-wrap">
-                            <PeopleTable visiblePeople={this.state.items} />
-                        </div>
+                    </div>
+                    <PersonHeader />
+                    <div className="people-table-wrap">
+                        <PeopleTable visiblePeople={items} />
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default SearchPage;

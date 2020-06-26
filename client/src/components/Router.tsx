7aee519347d0c7c898c2b3b74d404865783c1683
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-06-26 10:07:36 +0300

    Removed unnecessary return in component

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -12,24 +12,22 @@ import FamilyAdditionPage from '../pages/FamilyAdditionPage';
 import PersonAdditionPage from '../pages/PersonAdditionPage';
 import StatsPage from '../pages/StatsPage';
 
-const Router = () => {
-    return (
-        <BrowserRouter>
-            <Route path={'/'} component={Header} />
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/viewall" exact component={PeoplePage} />
-                <Route path="/viewall/families" exact component={FamiliesPage} />
-                <Route path="/person/:id" exact component={PersonPage} />
-                <Route path="/family/:id" exact component={FamilyPage} />
-                <Route path="/search" exact component={SearchPage} />
-                <Route path="/add/family" exact component={FamilyAdditionPage} />
-                <Route path="/add/person" exact component={PersonAdditionPage} />
-                <Route path="/statistics" exact component={StatsPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </BrowserRouter>
-    );
-};
+const Router = () => (
+    <BrowserRouter>
+        <Route path={'/'} component={Header} />
+        <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/viewall" exact component={PeoplePage} />
+            <Route path="/viewall/families" exact component={FamiliesPage} />
+            <Route path="/person/:id" exact component={PersonPage} />
+            <Route path="/family/:id" exact component={FamilyPage} />
+            <Route path="/search" exact component={SearchPage} />
+            <Route path="/add/family" exact component={FamilyAdditionPage} />
+            <Route path="/add/person" exact component={PersonAdditionPage} />
+            <Route path="/statistics" exact component={StatsPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>
+);
 
 export default Router;
